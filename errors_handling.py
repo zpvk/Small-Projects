181be8bbace4032fea29b2910a5be76cf92d9e17
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2020-05-23 19:37:51 +0530

    Zero DER

---
+++ errors_handling.py
@@ -0,0 +1,27 @@
+def party_planner(cookies, people):
+    leftovers = None
+    num_each = None
+    # TODO: Add a try-except block here to
+    #       make sure no ZeroDivisionError occurs.
+    try:
+        num_each = cookies // people
+        leftovers = cookies % people
+    except ZeroDivisionError:
+        print(' input a different number of people')
+
+    return(num_each, leftovers)
+
+# The main code block is below; do not edit this
+lets_party = 'y'
+while lets_party == 'y':
+
+    cookies = int(input("How many cookies are you baking? "))
+    people = int(input("How many people are attending? "))
+
+    cookies_each, leftovers = party_planner(cookies, people)
+
+    if cookies_each:  # if cookies_each is not None
+        message = "\nLet's party! We'll have {} people attending, they'll each get to eat {} cookies, and we'll have {} left over."
+        print(message.format(people, cookies_each, leftovers))
+
+    lets_party = input("\nWould you like to party more? (y or n) ")
